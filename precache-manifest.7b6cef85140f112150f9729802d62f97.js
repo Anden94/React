9043a17b2025b2d32004a5fa614e8967bb299cec
Andreas Nilsson <nilsson.94@live.se>
2020-09-23 14:31:12 +0200

    Updates

---
+++ precache-manifest.7b6cef85140f112150f9729802d62f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a51e3a5df5cf90bc21236316d70734c",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "a4d7c650b22e3b7f9bfc",
+    "url": "/React/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "fff7d14d19cd3864a25d",
+    "url": "/React/static/js/2.cb2e9514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.cb2e9514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d7c650b22e3b7f9bfc",
+    "url": "/React/static/js/main.6517df28.chunk.js"
+  },
+  {
+    "revision": "6ca0b992be1df16c9031",
+    "url": "/React/static/js/runtime-main.63d86472.js"
+  }
+]);
